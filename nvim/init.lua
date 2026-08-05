95f85e1349b9e80a48f7a45d620a04ebd38e5581
avasnova <avasnova@pm.me>
2026-08-05 15:06:05 +0300

    w.e.

--- nvim/init.lua
+++ nvim/init.lua
@@ -516,8 +516,9 @@ require("lazy").setup({
 
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
-					map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					--  To jump back, press <C-o>.
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition") -- keep for muscle memory
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
---
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon [A]dd file" })
+		vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "[H]arpoon menu" })
+
+		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
+		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon file 3" })
+		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon file 4" })
+		vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon file 5" })
+
+		-- Navigate without menu
+		vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end, { desc = "Harpoon prev" })
+		vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end, { desc = "Harpoon next" })
+	end,
+}
